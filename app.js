ef3e16d94c091631e8c4bcca87d009071ca43255
Alexandre Weber Dalabona <alexandre.dalabona@gmail.com>
2018-10-29 22:06:51 -0300

    formulários EJS

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ let express = require('express');
 let app = express();
 app.set('view engine', 'ejs');
 
-app.get('/tec', ((req, res) => {
-    res.render('secao/tecnologia');s
+app.get('/', ((req, res) => {
+    res.render('home/index');
+}));
+
+app.get('/formulario_inclusao_noticia', ((req, res) => {
+    res.render('admin/form_add_noticia');
+}));
+
+app.get('/noticias', ((req, res) => {
+    res.render('noticias/noticias');
 }));
 
 app.listen(3000, () => {
